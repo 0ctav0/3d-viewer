9fad05be0109b8e5b97708bd6c167c4c3f623de3
Anna Ra <anna.ra.phoenix@gmail.com>
2025-02-28 17:46:34 +0300

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
 import './App.css'
+import { Button, Stack } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
 
-    </>
+  return (
+    <Stack direction="row" height={"100%"}>
+      <Stack minWidth={300}>
+        // items...
+        <Stack direction="row" mt="auto">
+          <Button>Clear scene</Button>
+          <Button>Add group</Button>
+        </Stack>
+      </Stack>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </Stack>
   )
 }
 
